bf0f141a37c8fad21cfb73033f26b710138550a3
aluno <aluno@MSCGRESL04008.estacio.acad>
2017-08-31 21:23:10 -0300

    Aula 05

---
+++ src/Aula5/TesteConcatenacao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula5;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteConcatenacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String a = "String";
+        int b = 3;
+        int c = 7;
+        int d = 50;
+        
+        System.out.println(b + c + a + d);
+    }
+    
+}
