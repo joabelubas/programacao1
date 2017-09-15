b6265441c2204a35f87b390b8aeec02665744b56
aluno <aluno@MSCGRESL04018.estacio.acad>
2017-09-14 21:15:44 -0300

    Lista de Exercicos 1

---
+++ src/br/estacio/lista1/Exercicio01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.lista1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /** RESPOSTA: O processo de compilação é quando inserido o código fonte em linguagem Java onde assim é
+         * validada cada instrução e comando. Assim que o compilador não enxerguar nenhum erro de execução, é passado
+         * para o interpretador por meio de bytecode que funciona como um intermediário.  Sendo assim, o programa é 
+         * executado.
+        */
+    }
+    
+}
---
+++ src/br/estacio/lista1/Exercicio03.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.lista1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 26;
+        String  nome = "Joabe";
+        float peso = 76.8f;
+        char tipoSanguineo = 'A';
+        String dataNascimento = "29/05/1991";
+        int numeroCalca = 42;
+        String estadoCivil = "Solteiro";
+        
+    }
+    
+}
